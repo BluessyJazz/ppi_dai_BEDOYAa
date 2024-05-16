2bec787e3a234008928d43bfa2a79bc36f4809ad
BluessyJazz <112785703+BluessyJazz@users.noreply.github.com>
2024-05-15 20:14:09 -0500

    feat: Agregar inicialización del autenticador

--- modules/auth/__init__.py
+++ modules/auth/__init__.py
@@ -1,3 +1,34 @@
 """
 Este módulo contiene la lógica para la autenticación de usuarios.
 """
+
+import yaml
+from .authenticate import Authenticate
+from modules.db.conexion_db import ConexionDB
+from yaml.loader import SafeLoader
+
+
+def init_auth():
+    """
+    Inicializa la autenticación de usuarios.
+    """
+
+    # Cargar las credenciales de la base de datos
+    conexion_db = ConexionDB()
+
+    # Obtener los usuarios y sus credenciales
+    usuarios = conexion_db.obtener_usuarios()
+
+    # Cargar el archivo de configuración
+    with open("config.yaml", "r", encoding='utf-8') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    # Crear una instancia de la clase Authenticate
+    auth = Authenticate(
+        usuarios,
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days']
+    )
+
+    return auth
